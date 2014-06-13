53a16c3ea83dd2f6b45958473c94ca1f8af61b58
Laurent Laborde <kerdezixe@gmail.com>
2014-06-13 11:44:53 +0200

    fist working script

--- create-qemu-img.sh
+++ create-qemu-img.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-echo "another placeholder"
+#echo "another placeholder"
+
+platform=`uname`
+case "${platform}" in
+  Darwin)
+    dd if=/dev/zero of=bmfs.image bs=1m count=128
+    ;;
+  *)
+    dd if=/dev/zero of=bmfs.image bs=1M count=128
+    ;;
+esac
+
+./bin/bmfs bmfs.image format /force
+
+dd if=bootloader/Pure64/bmfs_mbr.sys of=bmfs.image bs=512 conv=notrunc
+dd if=bootloader/Pure64/pure64.sys of=bmfs.image bs=512 seek=16 conv=notrunc
+
 
 # Stole the code from Baremetal
 #cd bin
