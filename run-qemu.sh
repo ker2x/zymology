9ca42a98cee5f5877575dac5c1e48d819792deec
Laurent Laborde <kerdezixe@gmail.com>
2014-06-13 11:45:21 +0200

    it works

--- run-qemu.sh
+++ run-qemu.sh
@@ -3,5 +3,5 @@
 echo "This script will be used to run a qemu zymology image"
 
 # Stole the code from baremetal
-#qemu-system-x86_64 -vga std -smp 8 -m 256 -drive id=disk,file=bmfs.image,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "Zymology OS" -net nic,model=i82551
+qemu-system-x86_64 -vga std -smp 8 -m 256 -drive id=disk,file=bmfs.image,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "Zymology OS" -net nic,model=i82551
 
