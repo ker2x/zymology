009e64b0e14222053aefa1e8c1713eab693f71d7
Laurent Laborde <kerdezixe@gmail.com>
2014-06-16 16:53:26 +0200

    cleaning

--- bin/create-oak-img.sh
+++ bin/create-oak-img.sh
@@ -23,9 +23,3 @@ dd if=bootloader/Oak64/bin/mbr.sys of=bmfs.image bs=512 conv=notrunc
 #cat bootloader/Pure64/pure64.sys src/kernel.sys > os.sys
 #dd if=os.sys of=bmfs.image bs=512 seek=16 conv=notrunc
 #dd if=bootloader/Pure64/pure64.sys of=bmfs.image bs=512 seek=16 conv=notrunc
-
-# Stole the code from Baremetal
-#cd bin
-#echo Writing Pure64+Software
-#cat pure64.sys kernel64.sys > software.sys
-#dd if=software.sys of=bmfs.image bs=512 seek=16 conv=notrunc
